f07eb1e36cdc11af5922a386d1e7e5e25e6e7690
Adriani1992 <adrianikao119@gmail.com>
2023-09-07 23:36:00 +0800

    Learning
    Learning Vue3

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>{{ num }}</p>
+    <button type="button" @click.prevent="increment">Click me</button>
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let num = ref(0);
+
+    function increment() {
+      num.value++;
+    }
+
+    const user = reactive({
+      name: "Jenny",
+      age: 31,
+    });
+
+    setTimeout(() => {
+      user.name = "Leo";
+    }, 3000);
+
+    return {
+      num,
+      increment,
+      ...toRefs(user),
+    };
+  },
+};
+</script>
