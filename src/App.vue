4a0cc48148267341e4fbbd5a4752bba136e97002
Adriani1992 <adrianikao119@gmail.com>
2023-09-08 23:51:39 +0800

    #Practice D-2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
 <template>
   <div>
     <p>{{ num }}</p>
+    <p>{{ double }}</p>
     <button type="button" @click.prevent="increment">Click me</button>
     <p>{{ name }}</p>
+    <input type="text" v-model="phrase" />
+    <p>{{ reversePhrase }}</p>
   </div>
+  <Alert :user="user" />
 </template>
 
 <script>
-import { ref, reactive, toRefs } from "vue";
+import {
+  ref,
+  reactive,
+  toRefs,
+  watchEffect,
+  computed,
+  onBeforeMount,
+  onMounted,
+} from "vue";
+import Alert from "@/components/Alert.vue";
 export default {
   name: "App",
+  components: {
+    Alert,
+  },
   setup() {
+    onBeforeMount(() => {
+      console.log("onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("onMounted");
+    });
+
     let num = ref(0);
 
     function increment() {
       num.value++;
     }
+
+    const double = computed(() => {
+      return num.value * 2;
+    });
 
     const user = reactive({
       name: "Jenny",
@@ -26,10 +53,21 @@ export default {
       user.name = "Leo";
     }, 3000);
 
+    const phrase = ref("");
+    const reversePhrase = ref("");
+
+    watchEffect(() => {
+      reversePhrase.value = phrase.value.split("").reverse().join("");
+    });
+
     return {
       num,
       increment,
       ...toRefs(user),
+      phrase,
+      reversePhrase,
+      double,
+      user,
     };
   },
 };
---
+++ src/components/Alert.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="!flag">Hiiiiiiiii</div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  props: ["user"],
+  setup(props) {
+    const flag = computed(() => {
+      return props.user.name === "Leo";
+    });
+
+    console.log("props", props);
+
+    return {
+      flag,
+    };
+  },
+};
+</script>
+
