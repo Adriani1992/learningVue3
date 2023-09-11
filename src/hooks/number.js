e3945c07861c35b1770070341993f4f5441af841
Adriani1992 <adrianikao119@gmail.com>
2023-09-11 23:00:39 +0800

    #isRef && isReactive

--- src/hooks/number.js
+++ src/hooks/number.js
@@ -1,7 +1,18 @@
-import { ref, computed } from "vue"
+import { ref, computed, isRef, isReactive, reactive } from "vue"
+//isRef 可以被用來確認變數是否是ref產生的，同理isReactive 也是同等道理
 
 export const useNumber = () => {
+
+    const account = reactive({
+        saving: 3432,
+    })
+
+
+
     let num = ref(0);
+
+    console.log(isReactive(account));
+    console.log(isRef(num));
 
     function increment() {
         num.value++;
